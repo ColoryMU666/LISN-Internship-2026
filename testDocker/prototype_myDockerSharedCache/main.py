84b6aafdc1a1888f4f3ce553efe4574e404ab38a
ColoryMU666 <urios.matthieu@gmail.com>
2026-05-19 16:54:11 +0200

    Fixed a small bug to get the test to work

--- testDocker/prototype_myDockerSharedCache/main.py
+++ testDocker/prototype_myDockerSharedCache/main.py
@@ -5,8 +5,8 @@ VENV_LOCATION = "/tmp/venv/"
 BASE_DIR = os.getcwd()
 
 def main():
+    subprocess.run(f"uv venv {VENV_LOCATION}", shell=True)
     subprocess.run(f"cd {VENV_LOCATION}", shell=True)
-    subprocess.run("uv venv", shell=True)
     packages = []
     stop = False
     while not stop:

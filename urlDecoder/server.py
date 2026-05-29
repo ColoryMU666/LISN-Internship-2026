a76d4719576a7f0ec35c326398d0e077a5c04fdd
ColoryMU666 <urios.matthieu@gmail.com>
2026-05-29 16:42:57 +0200

    Added prototype of the url decoder

---
+++ urlDecoder/server.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import os
+import subprocess
+
+app = FastAPI()
+
+@app.post("/hello/")
+def hello():
+    return {"message" : "Hello world"}
+
+@app.post("/")
+def func(git_url, token, server_url=""):
+    git_url = f"https://{git_url}"
+    subprocess.run(f"uvx --with nbgitpuller --from jupyterlab sh -c \"gitpuller {git_url} main jserv && jupyter-lab --no-browser --ip=\"0.0.0.0\" --IdentityProvider.token={token} --allow-root jserv\"", shell=True)
+    return

2687d81adfca23dfc2f4a1dcec1c5df52518b5a0
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-01 14:12:44 +0200

    Not the usual forgotten parameter

--- urlDecoder/server.py
+++ urlDecoder/server.py
@@ -11,5 +11,5 @@ def hello():
 
 @app.get("/", response_class=RedirectResponse, status_code=302)
 def fn(shared_cache=""):
-    subprocess.run("uv run --with nbgitpuller --with jupyterlab jupyter-lab --port 8080")
+    subprocess.run("uv run --with nbgitpuller --with jupyterlab jupyter-lab --port 8080", shell=True)
     return "http://localhost:8080/lab"

2ac896f4f11130158a7e8e761e36fc829a2a230e
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-01 16:53:32 +0200

    Small fix

--- urlDecoder/server.py
+++ urlDecoder/server.py
@@ -30,7 +30,7 @@ def fn(shared_cache="", url="", token="", repo="", branch="main"):
     if jupyter_process is None or jupyter_process.poll() is not None:
         jupyter_process = subprocess.Popen(f"uv run --with nbgitpuller --with jupyterlab jupyter-lab --ip 0.0.0.0 --port 8080 --IdentityProvider.token={token} --allow-root", shell=True)
     if wait_for_port(port=8080, timeout=30):
-        subprocess.run(f"echo {url}/lab/hub/user-redirect/git-pull?token={token}&repo={repo}&branch={branch}", shell=True)
-        return f"{url}/lab/hub/user-redirect/git-pull?token={token}&repo={repo}&branch={branch}"
+        subprocess.run(f"echo {url}lab/hub/user-redirect/git-pull?token={token}&repo={repo}&branch={branch}", shell=True)
+        return f"{url}lab/hub/user-redirect/git-pull?token={token}&repo={repo}&branch={branch}"
     else:
         {"error" : "Jupyterlab did not start in time"}

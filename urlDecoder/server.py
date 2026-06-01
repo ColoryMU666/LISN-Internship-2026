ba9f473abacd1cd35563ab234e1df5074e8dc8a1
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-01 16:02:50 +0200

    Small fix

--- urlDecoder/server.py
+++ urlDecoder/server.py
@@ -25,11 +25,11 @@ def hello():
     return {"message" : "Hello world"}
 
 @app.get("/", response_class=RedirectResponse, status_code=302)
-def fn(shared_cache="", url=""):
+def fn(shared_cache="", url="", token=""):
     global jupyter_process
     if jupyter_process is None or jupyter_process.poll() is not None:
-        jupyter_process = subprocess.Popen("uv run --with nbgitpuller --with jupyterlab jupyter-lab --ip 0.0.0.0 --port 8080 --allow-root", shell=True)
+        jupyter_process = subprocess.Popen("uv run --with nbgitpuller --with jupyterlab jupyter-lab --ip 0.0.0.0 --port 8080 --IdentityProvider.token={{PASSWORD}} --allow-root", shell=True)
     if wait_for_port(port=8080, timeout=30):
-        return url
+        return f"{url}?token={token}"
     else:
         {"error" : "Jupyterlab did not start in time"}

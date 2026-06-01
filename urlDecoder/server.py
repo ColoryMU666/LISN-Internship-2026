9545ff1e30301c474e11828695f301ec60656642
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-01 14:11:35 +0200

    Now we prayge 🙏

--- urlDecoder/server.py
+++ urlDecoder/server.py
@@ -10,5 +10,6 @@ def hello():
     return {"message" : "Hello world"}
 
 @app.get("/", response_class=RedirectResponse, status_code=302)
-def fn(git_url=""):
-    return git_url
+def fn(shared_cache=""):
+    subprocess.run("uv run --with nbgitpuller --with jupyterlab jupyter-lab --port 8080")
+    return "http://localhost:8080/lab"

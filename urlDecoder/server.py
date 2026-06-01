7b1f6ba129cca115b79ca8cb24f7269291a76291
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-01 12:04:20 +0200

    Added comments for testing purposes

--- urlDecoder/server.py
+++ urlDecoder/server.py
@@ -9,8 +9,8 @@ app = FastAPI()
 def hello():
     return {"message" : "Hello world"}
 
-@app.post("/")
-def func(git_url, token, server_url=""):
-    git_url = f"https://{git_url}"
-    subprocess.run(f"uvx --with nbgitpuller --from jupyterlab sh -c \"gitpuller {git_url} main jserv && jupyter-lab --no-browser --ip=\"0.0.0.0\" --IdentityProvider.token={token} --allow-root jserv\"", shell=True)
-    return
+#@app.post("/")
+#def func(git_url, token, server_url=""):
+#    git_url = f"https://{git_url}"
+#    subprocess.run(f"uvx --with nbgitpuller --from jupyterlab sh -c \"gitpuller {git_url} main jserv && jupyter-lab --no-browser --ip=\"0.0.0.0\" --IdentityProvider.token={token} --allow-root jserv\"", shell=True)
+#    return

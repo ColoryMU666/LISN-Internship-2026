ed35e766ed9423931dde2adffcdb8c4d81643d04
ColoryMU666 <urios.matthieu@gmail.com>
2026-06-25 13:43:54 +0200

    Removed tmp dir

--- tmp/server.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
-
-app =  FastAPI()
-
-@app.get("/test/", response_class=PlainTextResponse)
-def fn(request: Request):
-    return f"RAW URL: {request.url}\nPARAMS: {dict(request.query_params)}"
-     
